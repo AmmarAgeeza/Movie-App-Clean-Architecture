71c809aa177b5293fe5d27e266c74b187175276b
AmmarAgeeza <ammarfathy516@gmail.com>
2023-03-19 22:24:13 +0200

    add movie remote data source

---
+++ lib/movies/data/datasource/movie_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:movie_app_clean_architecture/movies/data/models/movie_model.dart';
+
+class MovieRemoteDataSource {
+  Future<List<MovieModel>> getNowPlayingMovies() async {
+    final response = await Dio().get(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=5ba40f59e6a8325930ebab075ccd9e9d');
+    if (response.statusCode == 200) {
+      return List<MovieModel>.from(
+        response.data['results']
+            .map(
+              (e) => MovieModel.fromJson(e),
+            )
+            .toList(),
+      );
+    } else {
+      return [];
+    }
+  }
+}
